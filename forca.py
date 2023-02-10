a5e00c75e69f7cefaef7cadc631b65e3bd67785f
Eduarda Schüler Costa <eduarda.schuler.costa@gmail.com>
2023-02-10 14:55:19 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -5,8 +5,42 @@ def define_palavra():
     index = random.randint(0, qtd_palavras)
     palavra_secreta = palavras[index]
     return palavra_secreta
-    
+
+def define_nivel():
+    print("Em qual nível você deseja jogar?")
+    print("(1)Fácil  (2)Médio  (3)Difícil")
+    nivel = int(input("Insira o número:"))
+    if nivel == 1:
+        max_erros = 10
+    elif nivel == 2:
+        max_erros = 6
+    else:
+        max_erros = 3
+    return max_erros
+
+def testa_encerra(pAcertos, pQtd_letras, pErros, pMax_erros):
+    if (pAcertos == pQtd_letras) and (pErros < pMax_erros):
+        encerra = True
+    elif (pAcertos < pQtd_letras) and (pErros == pMax_erros):
+        encerra = True
+    else: 
+        encerra = False
+    return encerra
+
+def testa_perdeu(pAcertos, pQtd_letras, pErros, pMax_erros):
+    if (pAcertos < pQtd_letras) and (pErros >= pMax_erros):
+        perdeu = True
+    else:
+        perdeu = False
+    return perdeu
+
 def mostra_palavra(pQtd_letras,pValores):
+    print(" ________")
+    print("|        |")
+    print("|         ")
+    print("|         ")
+    print("|         ")
+    print("|         ")
     print("Palavra:")
     r = 0
     while r < pQtd_letras:
@@ -19,34 +53,52 @@ def jogar():
     print("Bem vindo ao jogo de forca!")
     print("*********************************")
 
+    
+    perdeu = False
+    encerra = False
+    pontos = 1000
+    erros = 0
+    acertos = 0
+    max_erros = define_nivel()
+    
     palavra_secreta = define_palavra()
     qtd_letras = len(palavra_secreta)
     valores = []
-    perdeu = False
-    acertou = False
-    pontos = 1000
-    tentativas = 1
-
-    print("Em qual nível você deseja jogar?")
-    print("(1)Fácil  (2)Médio  (3)Difícil")
-    nivel = int(input("Insira o número:"))
-    r = 0
-    while r < qtd_letras:
+    letras_usadas = set()
+    for letras in palavra_secreta:
         valores.append("_")
-        r += 1
-    while (not perdeu) and (not acertou):
+
+    while (not encerra):
         chute = input("Digite uma letra:")
         chute = chute.strip()
+        chute = chute.upper()
+        if chute in letras_usadas:
+            print("Você já chutou esta letra. Tente outra.")
+            print("Letras usadas:")
+            print(letras_usadas)
+            continue
+        else:
+            letras_usadas.add(chute)
+            print("Letras usadas:")
+            print(letras_usadas)
         index = 0
         encontrou = False
         for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
+            if chute == letra.upper():
                 encontrou = True
                 valores[index] = chute
+                acertos += 1 
             index += 1
         if not encontrou:
             print("Não encontrei esta letra")
+            erros +=1
         mostra_palavra(qtd_letras,valores)
+        encerra = testa_encerra(acertos, qtd_letras, erros, max_erros)
+        perdeu = testa_perdeu(acertos, qtd_letras, erros, max_erros)
+    if perdeu:
+        print("Você perdeu!")
+    else:
+        print("Você ganhou!")
     print("Fim do jogo!")
 
 if (__name__ == "__main__"):
