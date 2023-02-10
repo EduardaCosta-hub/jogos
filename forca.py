94ac4d50448e7e0fba46af3d5145cd08ff930264
Eduarda Schüler Costa <eduarda.schuler.costa@gmail.com>
2023-02-10 17:32:03 -0300

    updates

--- forca.py
+++ forca.py
@@ -3,7 +3,7 @@ import os
 from unittest import case
 def define_palavra():
     palavras = []
-    os.chdir('Frutas.txt')
+    os.chdir("c:\Users\EduardaCosta\PycharmProjects\jogos")
     with open("Frutas.txt") as arquivo:
         conteudo = arquivo.read()
         for linha in conteudo:
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import forca
 import adivinhacao
+import os
 print("*********************************")
 print("*****Olá! Escolha o seu jogo*****")
 print("*********************************")
-
+print(os.getcwd())
 print("(1)Adivinhação  (2)Forca")
 jogo = int(input("Digite o número:"))
 if jogo == 1:
