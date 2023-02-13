eba9ba90614a006310cd151c10511db6e11e79d7
EduardaCosta-hub <eduarda.schuler.costa@gmail.com>
2023-02-13 11:36:08 -0300

    Update imagens_forca.py

---
+++ imagens_forca.py
@@ -0,0 +1,79 @@
+def img0():
+    print(" _________")  
+    print("|         |")
+    print("|         |")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+
+def img1():
+    print(" _________")  
+    print("|         |")
+    print("|        _|_")
+    print("|       (   )")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+
+def img2():
+    print(" _________")  
+    print("|         |")
+    print("|        _|_")
+    print("|       (x x)")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+
+def img3():
+    print(" _________")  
+    print("|         |")
+    print("|        _|_")
+    print("|       (x_x)")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+
+def img4():
+    print(" _________")  
+    print("|         |")
+    print("|        _|_")
+    print("|       (x_x)")
+    print("|        [ ]")
+    print("|")
+    print("|")
+    print("|")
+
+def img5():
+    print(" _________")  
+    print("|         |")
+    print("|        _|_")
+    print("|       (x_x)")
+    print("|       |[ ]|")
+    print("|")
+    print("|")
+    print("|")
+
+def img6():
+    print(" _________")  
+    print("|         |")
+    print("|        _|_")
+    print("|       (x_x)")
+    print("|       |[ ]|")
+    print("|        [ ]")
+    print("|")
+    print("|")
+
+def img7():
+    print(" _________")  
+    print("|         |")
+    print("|        _|_")
+    print("|       (x_x)")
+    print("|       |[ ]|")
+    print("|        [ ]")
+    print("|        | |")
+    print("|")
